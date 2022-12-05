2ab3b05ee403c9dced9d23d0898f7fdb46640918
Joel Hardamon <jchardamon@gmail.com>
2022-12-04 22:19:59 -0500

    Updates to database, source, and html code.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
-function fetch() {
+function fetchMovies() {
     // To pass the tests, don't forget to return your fetch!
-    return fetch('')
-    .then(resp => resp.json())
-    .then(bookData => renderBooks(bookData));
-  }
-  
-  function renderMovie(movies) {
-    const main = document.querySelector('main');
-    movies.forEach(book => {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = book.name;
-      main.appendChild(h2);
-    });
-  }
+    fetch('http://localhost:3000/Movies')
+    .then(response => response.json())
+    .then(movieData => renderMovies(movieData));
+  };
 
-  function renderTvShow(tvShows) {
-    const main = document.querySelector('main');
-    tvShows.forEach(tvShow => {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = tvShows.name;
-      main.appendChild(h2);
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    fetchMovie();
+function fetchSeries() {
+  // 
+  fetch('http://localhost:3000/Series')
+  .then(response => response.json())
+  .then(seriesData => renderSeries(seriesData));
+};
+
+function fetchList() {
+  // 
+  fetch('http://localhost:3000/Home')
+  .then(resp => resp.json())
+  .then(seriesData => renderSeries(seriesData));
+};
+
+function renderList(list){
+  const main = document.querySelector('main');
+  list.forEach(list => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = list.name;
+    main.appendChild(h2);
+});
+}
+
+function renderList(movies) {
+  const main = document.querySelector('main');
+  movies.forEach(movie => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = movie.name;
+    main.appendChild(h2);
   });
-  
+};
+
+function renderMovies(movies) {
+  const main = document.querySelector('main');
+  list.forEach(list => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = list.name;
+    main.appendChild(h2);
+  });
+};
+
+function renderSeries(series) {
+  const main = document.querySelector('main');
+  series.forEach(series => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = series.name;
+    main.appendChild(h2);
+  });
+};
+
+const init = () => {
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = document.querySelector('input#searchByID');
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+      });
+    });
+  };
+ 
+document.addEventListener('DOMContentLoaded', init);
