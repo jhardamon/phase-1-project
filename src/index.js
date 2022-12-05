907881817111f4238e3b2f4c632de09586288731
Joel Hardamon <jchardamon@gmail.com>
2022-12-05 02:58:03 -0500

    Revisions of syntax.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function fetchMovies() {
-    // To pass the tests, don't forget to return your fetch!
+    // 
     fetch('http://localhost:3000/Movies')
     .then(response => response.json())
     .then(movieData => renderMovies(movieData));
@@ -14,13 +14,13 @@ function fetchSeries() {
 
 function fetchList() {
   // 
-  fetch('http://localhost:3000/Home')
+  fetch('http://localhost:3000/')
   .then(resp => resp.json())
   .then(seriesData => renderSeries(seriesData));
 };
 
 function renderList(list){
-  const main = document.querySelector('main');
+  const main = document.querySelector('mainPage');
   list.forEach(list => {
     const h2 = document.createElement('h2');
     h2.innerHTML = list.name;
@@ -60,10 +60,10 @@ const init = () => {
   inputForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const input = document.querySelector('input#searchByID');
-    fetch(`http://localhost:3000/movies/${input.value}`)
+    fetch(`http://localhost:3000/`)
     .then(response => response.json())
     .then(data => {
-      const title = document.querySelector('section#movieDetails h4');
+      const title = document.querySelector('section#searchEngine h4');
       const summary = document.querySelector('section#movieDetails p');
       title.innerText = data.title;
       summary.innerText = data.summary;
