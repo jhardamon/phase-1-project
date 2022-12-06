e2b1edb3639f0803222f32df13bd5ad5668ce2c2
Joel Hardamon <jchardamon@gmail.com>
2022-12-06 00:59:30 -0500

    Various changes.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const init = () => {
   fetch(`http://localhost:3000`)
   .then(response => response.json())
   .then(data => {
-    // buildSortOptionsToolBar();
-    // fetchAll();
+  //  buildSortOptionsToolBar();
+    fetchAll();
      });
   };
 document.addEventListener('DOMContentLoaded', init);
@@ -32,55 +32,55 @@ document.addEventListener('DOMContentLoaded', init);
 
 
 
-// function buildSortOptionsToolBar() {
-//   //Multi-use reference Variables
-//   const capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-//   const wordsAndPhrases = ["#","All","Movies","Series","Dark Mode","Light Mode"]
-//   const sortOptionsIcons = [...capitalLetters,...wordsAndPhrases]
-//   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//   const numbers = ['0','1','2','3','4','5','6','7','8','9'];
-//   const alphanumeric = [...alphabet, ...numbers];
-//   const sortOptions = document.querySelector("div#sortOptions");
+function buildSortOptionsToolBar() {
+  //Multi-use reference Variables
+  const capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const wordsAndPhrases = ["#","All","Movies","Series","Dark Mode","Light Mode"]
+  const sortOptionsIcons = [...capitalLetters,...wordsAndPhrases]
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+  const alphanumeric = [...alphabet, ...numbers];
+  const sortOptions = document.querySelector("div#sortOptions");
 
-//   const buildSearchBar = () => {
-//     const searchBarForm = document.createElement("form");
-//     searchBarForm.setAttribute('name','Search');
+  const buildSearchBar = () => {
+    const searchBarForm = document.createElement("form");
+    searchBarForm.setAttribute('name','Search');
     
-//     const searchInput = document.createElement("input");
-//     searchBarForm.appendChild(searchInput);
-//     searchInput.setAttribute('name','Search');
-//     searchInput.setAttribute('type','TEXT')
-//     searchInput.setAttribute('value','Search')
-//     sortOptions.appendChild(searchBarForm);
-//     ;}
+    const searchInput = document.createElement("input");
+    searchBarForm.appendChild(searchInput);
+    searchInput.setAttribute('name','Search');
+    searchInput.setAttribute('type','TEXT')
+    searchInput.setAttribute('value','Search')
+    sortOptions.appendChild(searchBarForm);
+    ;}
 
-//   for (let option of sortOptionsIcons ){
-//     sortOptionsElement = document.createElement('a');
-//     sortOptionsElement.setAttribute('class', 'topBarUnclicked');
-//     sortOptionsElement.setAttribute('href', '#');
-//     sortOptionsElement.setAttribute('textContent',`${option}`);
+  for (let option of sortOptionsIcons ){
+    sortOptionsElement = document.createElement('a');
+    sortOptionsElement.setAttribute('class', 'topBarUnclicked');
+    sortOptionsElement.setAttribute('href', '#');
+    sortOptionsElement.setAttribute('textContent',`${option}`);
     
-//     document.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       event.setAttribute("class","topBarClicked")
-//       if (option in wordsAndPhrases){
+    document.addEventListener("click", (event) => {
+      event.preventDefault();
+      event.setAttribute("class","topBarClicked")
+      if (option in wordsAndPhrases){
 
-//       } 
-//       else if (option in capitalLetters)
-//          sortByStartingCharacter(option);
-//     }
-//   )
-// };
-// buildSearchBar();
+      } 
+      else if (option in capitalLetters)
+         sortByStartingCharacter(option);
+    }
+  )
+};
+buildSearchBar();
 
-// };
+};
 //Referenced for page manipulation
 const item = document.querySelector("div#item");
 const mainPageForList = document.querySelector("div#mainPage");
 
 //GET requests and list creation
 function fetchMovies() {
-  fetch('http://localhost:3000/all/')
+  fetch('http://localhost:3000/all')
   .then(response => response.json())
   .then(data => renderMovies(data))
   .catch(error => console.log(error));
@@ -97,7 +97,7 @@ function renderMovies(list) {
 
     const img = document.createElement('img');
     img.setAttribute('class','mainPhotos');
-    img.setAttribute('src',`${movie.image}`);
+    img.setAttribute('src',`src/photos/moviePhotos/${movie.name}.jpg`);
 
     const p = document.createElement('p');
     p.setAttribute('value',`${movie.description}\n Director:${movie.director} \n Genre(s):${movie.genres} \n Ranking:${movie.ranking} \n Year Released:${movie["Year Released"]}`);
@@ -108,7 +108,7 @@ function renderMovies(list) {
 };
 //
 function fetchSeries() {
-  fetch('http://localhost:3000/all/series')
+  fetch('http://localhost:3000/all')
   .then(response => response.json())
   .then(data => renderSeries(data))
   .catch(error => console.log(error));
@@ -126,8 +126,7 @@ function renderSeries(list) {
     const img = document.createElement('img');
     img.setAttribute('class','mainPhotos');
     console.log(series.image);
-    img.setAttribute('href',`${series.image}`)
-
+    img.setAttribute('src',`src/photos/seriesPhotos/${series.name}.jpg`)
     const p = document.createElement('p');
     p.setAttribute('value',`${series.description}\n Director:${series.director} \n Genre(s):${series.genres} \n Ranking:${series.ranking} \n Seasons info:${series["seasons"]}`);
     div.appendChild(h2);
@@ -144,7 +143,7 @@ function fetchAll() {
   };
 
 function renderAll(data) {
-  const movieObject = data.all.amovies;
+  const movieObject = data.all.movies;
   const seriesObject = data.all.series;
 
   renderMovies(movieObject);
