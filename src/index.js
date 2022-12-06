d80204290298913b075da3b535b769265ab35f37
Joel Hardamon <jchardamon@gmail.com>
2022-12-06 00:09:08 -0500

    Various updates to webpage.

--- src/index.js
+++ src/index.js
@@ -1,90 +1,155 @@
-// const init = () => {
-//   const inputForm = document.querySelector('form')
-//   inputForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const input = document.querySelector('input#myInput');
-//     fetch(`http://localhost:3000/`)
-//     .then(response => response.json())
-//     .then(data => {
-//       const title = document.querySelector('h1');
-//       const summary = document.querySelector('');
-//       title.innerText = data.title;
-//       summary.innerText = data.summary;
-//       });
-//     });
-//   };
- 
-// document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+  fetch(`http://localhost:3000`)
+  .then(response => response.json())
+  .then(data => {
+    // buildSortOptionsToolBar();
+    // fetchAll();
+     });
+  };
+document.addEventListener('DOMContentLoaded', init);
+
+
 
 //Sorting by starting alphanumeric character
-function sortByStartingCharacter(textInput){
-  if (textInput.startsWith() in capitalLetters || numbers){
+
+// function sortByStartingCharacter(textInput){  
+//   textInput.filter("",
+//   () => (if (character in numbers){
+
+//   }
+
+//   else if (character in capitalLetters){
+//     character = character.toLowerCase();
+
+
+//   }
+
+//   else{
+
+//     };))
+
+// }
+
+
+
+// function buildSortOptionsToolBar() {
+//   //Multi-use reference Variables
+//   const capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//   const wordsAndPhrases = ["#","All","Movies","Series","Dark Mode","Light Mode"]
+//   const sortOptionsIcons = [...capitalLetters,...wordsAndPhrases]
+//   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//   const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+//   const alphanumeric = [...alphabet, ...numbers];
+//   const sortOptions = document.querySelector("div#sortOptions");
+
+//   const buildSearchBar = () => {
+//     const searchBarForm = document.createElement("form");
+//     searchBarForm.setAttribute('name','Search');
     
-  }
-}
+//     const searchInput = document.createElement("input");
+//     searchBarForm.appendChild(searchInput);
+//     searchInput.setAttribute('name','Search');
+//     searchInput.setAttribute('type','TEXT')
+//     searchInput.setAttribute('value','Search')
+//     sortOptions.appendChild(searchBarForm);
+//     ;}
 
-const buildSortOptionsToolBar = () => {
-  //
-  capitalLetters = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-  sortOptionsIcons = ["All",...capitalLetters,"#","Movies","Series","Dark Mode","Light Mode"]
-  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  numbers = ['0','1','2','3','4','5','6','7','8','9']
-  for (let option of sortOptionsIcons ){
-    const sortOptions = document.querySelector("div#sortOptions");
-    sortOptionsElement = sortOptions.createElement('a');
-    sortOptionsElement.setAttribute('class', 'topBarUnclicked');
-    sortOptionsElement.setAttribute('href', '#');
-    sortOptionsElement.setAttribute('textContent',`${option}`);
-    document.addEventListener("click", )
-};
-  
-};
+//   for (let option of sortOptionsIcons ){
+//     sortOptionsElement = document.createElement('a');
+//     sortOptionsElement.setAttribute('class', 'topBarUnclicked');
+//     sortOptionsElement.setAttribute('href', '#');
+//     sortOptionsElement.setAttribute('textContent',`${option}`);
+    
+//     document.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       event.setAttribute("class","topBarClicked")
+//       if (option in wordsAndPhrases){
 
+//       } 
+//       else if (option in capitalLetters)
+//          sortByStartingCharacter(option);
+//     }
+//   )
+// };
+// buildSearchBar();
+
+// };
+//Referenced for page manipulation
+const item = document.querySelector("div#item");
+const mainPageForList = document.querySelector("div#mainPage");
+
+//GET requests and list creation
 function fetchMovies() {
-    // 
-  fetch('http://localhost:3000/Movies')
+  fetch('http://localhost:3000/all/')
   .then(response => response.json())
-  .then(movieData => renderMovies(movieData));
+  .then(data => renderMovies(data))
+  .catch(error => console.log(error));
   };
 
-  function renderMovies(movies) {
-    const main = document.querySelector('div#mainPage');
-    movies.forEach(list => {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = list.name;
-      main.appendChild(h2);
-    });
-  };
+function renderMovies(list) {
+  list.movies.forEach(movie => {
 
-function fetchSeries() {
-  // 
-  fetch('http://localhost:3000/Series')
-  .then(response => response.json())
-  .then(seriesData => renderSeries(seriesData));
-};
+    const div = document.createElement('div');
+    div.setAttribute('class','item')
 
-function renderSeries(series) {
-  const main = document.querySelector('div#mainPage');
-  series.forEach(series => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = series.name;
-    main.appendChild(h2);
+    const h2 = document.createElement('h1');
+    h2.innerHTML = movie.name;
+
+    const img = document.createElement('img');
+    img.setAttribute('class','mainPhotos');
+    img.setAttribute('src',`${movie.image}`);
+
+    const p = document.createElement('p');
+    p.setAttribute('value',`${movie.description}\n Director:${movie.director} \n Genre(s):${movie.genres} \n Ranking:${movie.ranking} \n Year Released:${movie["Year Released"]}`);
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(p)
   });
 };
+//
+function fetchSeries() {
+  fetch('http://localhost:3000/all/series')
+  .then(response => response.json())
+  .then(data => renderSeries(data))
+  .catch(error => console.log(error));
+  };
 
-function fetchList() {
-  // 
-  fetch('http://localhost:3000/')
-  .then(resp => resp.json())
-  .then(seriesData => renderSeries(seriesData));
+function renderSeries(list) {
+  list.series.forEach(series => {
+
+    const div = document.createElement('div');
+    div.setAttribute('class','item')
+
+    const h2 = document.createElement('h1');
+    h2.innerHTML = series.name;
+
+    const img = document.createElement('img');
+    img.setAttribute('class','mainPhotos');
+    console.log(series.image);
+    img.setAttribute('href',`${series.image}`)
+
+    const p = document.createElement('p');
+    p.setAttribute('value',`${series.description}\n Director:${series.director} \n Genre(s):${series.genres} \n Ranking:${series.ranking} \n Seasons info:${series["seasons"]}`);
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(p)
+  });
 };
+//Fetch all
+function fetchAll() {
+  fetch('http://localhost:3000/all/')
+  .then(response => response.json())
+  .then(data => renderAll(data))
+  .catch(error => console.log(error));
+  };
 
-function renderList(list){
-  const main = document.querySelector('div#mainPage');
-  list.forEach(list => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = list.name;
-    main.appendChild(h2);
-});
+function renderAll(data) {
+  const movieObject = data.all.amovies;
+  const seriesObject = data.all.series;
+
+  renderMovies(movieObject);
+  renderSeries(seriesObject);
 }
+//Update requests
 
+//Patch requests
